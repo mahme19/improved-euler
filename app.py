7ef3536343e2c7a4c20fa8dee1924d5b5d3ef29e
mustafa <mahme19@student.sdu.dk>
2020-12-02 12:58:49 +0100

    implemented eulers improved method

--- app.py
+++ app.py
@@ -1,42 +1,35 @@
-import math
 import matplotlib
 import matplotlib.pyplot as plt
 
-def df(y,t):
-    return (y*(1-math.pow(y, 2)))
+def f(y):
+    return (y*(1-pow(y,2)))
 
 
-def funcf(y, t):
-    return (math.log(y)-1/2*math.log(y+1)-1/2*math.log(y-1))
-
-
-stepsize = 1
-maxiter = 1000
-current_y = -0.000001
+h = 0.05
+maxiter = 1000000
 lastStepsize = 1
-euler_t = 0
-current_t = 1
-precision = 0.00001
+current_y = -0.1 
+current_t = 0 
+precision = 0.000001  
 i = 0
 t = []
 y = []
 
-while(lastStepsize > precision and maxiter > i):
+while(maxiter > i):
     i = i+1
-    previous_y = current_y
-    current_y = previous_y+stepsize
-    prev_euler_t = euler_t
-    euler_t = previous_y + (stepsize * df(previous_y, prev_euler_t))
     previous_t = current_t
-    current_t = previous_t + (stepsize * ((df(previous_y, previous_t) + df(current_y, euler_t))/2))
+    current_t = previous_t + h 
+    previous_y = current_y
+    current_y = previous_y + h * ((f(previous_y) + f(previous_y + h * f(previous_y)))/2)
     y.append(current_y)
     t.append(current_t)
-    lastStepsize = abs(current_y-previous_y)
+    lastStepsize = abs(previous_t - current_t)
+
+print(current_y)
+print(current_t)
+print(i)
 
 plt.plot(t,y)
-plt.show()    
-
-
-
+plt.show()
 
                 
